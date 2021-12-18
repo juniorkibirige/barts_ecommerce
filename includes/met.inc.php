ce295536889f802b77816cf5804789f62885b40c
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-12-18 16:13:53 +0300

    better redirect

--- includes/met.inc.php
+++ includes/met.inc.php
@@ -1,9 +1,10 @@
 <?php 
 if ($_SERVER['SERVER_PORT'] == 80) {
-  $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-  header('HTTP/1.1 301 Moved Permanently');
-  header('Location: ' . $location);
-  exit;
+  // $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  // header('HTTP/1.1 301 Moved Permanently');
+  // header('Location: ' . $location);
+  // exit;
+  echo '<script>alert("Server not secure!")</script>';
 }
 ?>
 <meta property="og:type" content="website">
